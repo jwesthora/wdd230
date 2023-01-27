7593a66b59a93a640464e3479491ba03ce21d6c5
jwesthora <jud05001@byui.edu>
2023-01-27 15:33:58 -0800

    making grid

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,4 +1,4 @@
-const h1 = document.querySelector('h1');
+//const h1 = document.querySelector('h1');//
 // Select the HTML element to manipulate
 const date1 = document.querySelector("#date1");
 const message = document.querySelector("#emessage");
@@ -50,16 +50,13 @@ const time_minutes= d.getMinutes();
 const time_seconds = d.getSeconds();
 const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year} `;
 document.querySelector("#date1").textContent = fulldate;
-document.getElementById("year").innerHTML = `${year}`;
+document.getElementById("year").innerHTML =`${year}`;
 `Current Date: ${dayName}, ${monthName} ${d.getDate()}, ${year}`;
 
 
-// Store the selected elements that we are going to use. 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
-
-// Add a simple arrow function that listens for the <li> hamburger button click event.
-// When clicked, the <ul class="navigation">'s class list toggle'
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// What does toggle mean? Instead of add and remove, toggle means add the class name (the parameter, which in this case is named 'responsive') if it does not currently exist, and remove the 
+function toggleMenu() {
+	document.getElementById("primaryNav").classList.toggle("open");
+	document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+	const x = document.getElementById('hamburgerBtn')
+	x.onlick = toggleMenu;
