7a447e3f9fd32c7c2a29a705793e37456e772e6b
jwesthora <jud05001@byui.edu>
2023-02-06 18:51:49 -0800

    heading and add date

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,48 +1,22 @@
-// Date
 
-const date = new Date();
+// date for header
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
 
-const day = date.toLocaleDateString('en', { day: 'numeric' });
-const weekday = date.toLocaleDateString('en', { weekday: 'long' });
-const month = date.toLocaleDateString('en', { month: 'long' });
-const year = date.toLocaleDateString('en', { year: 'numeric' });
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
 
-document.querySelector('#nav-date').textContent = `${weekday}, ${day} ${month} ${year}`;
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
-document.querySelector('#year').textContent = date.getFullYear();
-
-document.querySelector('#footer-date').textContent = date.toLocaleString('en-GB');
-const dayname = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-const d = new Date();
-const dayName = dayname[d.getDay()];
-const monthName = months[d.getMonth()];
-
-
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-document.querySelector("#datec").textContent = fulldate;
-document.querySelector("year").textContent = `${year}`;
 
 
 
