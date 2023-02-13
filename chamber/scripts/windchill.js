bc3a9e0d2fd7cde44306acb6b4a7f557731131be
jwesthora <jud05001@byui.edu>
2023-02-13 14:20:52 -0800

    week #4 changes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -2,9 +2,6 @@ function drawWeather( d ) {
     var celcius = Math.round(parseFloat(d.main.temp)-273.15);
       var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
       
-      //document.getElementById('description').innerHTML = d.weather[0].description;
-      document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
-      document.getElementById('location').innerHTML = d.name;
     const windSpeed = d.wind.speed;
     const mph =  parseFloat(windSpeed * 2.236936).toFixed(1);
     document.getElementById('ws').innerHTML = `Wind Speed: ${mph} mph`
