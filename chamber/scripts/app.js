11c2c3319c83d7d5922dca5c1b9f131e9ee71d5e
jwesthora <jud05001@byui.edu>
2023-02-16 13:33:42 -0800

    lazy loading

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -47,3 +47,39 @@ if (now.getDay() === 1 || now.getDay() == 2){
     document.getElementById("banner").style.display = "none"
   //}
 
+
+// java for Discover page
+// get all img with data src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//optional parameters being set for the intersection observer
+const imgoptions= {
+  threshold: 0
+}
+
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {image.removeAttribute("data-src");};
+};
+// first check to see if intersection observer is supported 
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    // Loop through each img on check status and loas if necessary
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+    // load all images if not supported
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+  
