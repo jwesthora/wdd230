3caaa01ed21974fa4c329ada409848b11ef92c2a
jwesthora <jud05001@byui.edu>
2023-02-22 16:04:13 -0800

    join

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -119,3 +119,17 @@ let daysleft = (christmas.getTime() - Date.now()) / 84600000;
 daysLeftOutput.innerHTML = `${daysleft.toFixed(
 	0
 )} days<br> 🎄Christmas is ${christmas}`;
+
+
+
+// hidden date and time for join.html
+var d = new Date();
+
+// Set the value of the "date" field
+document.getElementById("date").value = d.toDateString();
+
+// Set the value of the "time" field
+var hours = d.getHours();
+var mins = d.getMinutes();
+var seconds = d.getSeconds();
+document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
