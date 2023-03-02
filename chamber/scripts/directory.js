858a865b6d32f51c89e2242aeab143e02621291d
jwesthora <jud05001@byui.edu>
2023-03-02 13:23:26 -0800

    Fixing errors

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,57 @@
+const url = 'https://jwesthora.github.io/wdd230/chamber/data.json';
+
+
+async function getCompaniesData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.companies); // reference the prophet array of data object
+    displayCompanies(data.companies);
+}
+
+
+
+const displayCompanies = (companies) => {
+const cards = document.querySelector('div.cards'); // select the output container element
+  
+companies.forEach((company) => {
+ // Create elements to add to the div.cards element
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+    let bPhone = document.createElement('p');
+    let bEmail = document.createElement('p');
+    let bAddress = document.createElement('p');
+    
+  
+// Build the h2 content out to show the prophet's full name - finish the template string
+    h2.textContent = `${company.bName}`;
+
+// Build the p content to show prophet's birth date and birth place
+bPhone.textContent = `Phone: ${company.bPhone}`;
+bEmail.textContent = `Contact: ${company.bEmail}`;
+bAddress.textContent = `Address: ${company.bAddress}`;
+
+ // Build the image portrait by setting all the relevant attribute
+    portrait.setAttribute('src', company.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${company.bName} ______________`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '300');
+    portrait.setAttribute('height', '200');
+  
+// Append the section(card) with the created elements
+ card.appendChild(h2);
+ card.appendChild(portrait);
+ card.appendChild(bPhone);
+ card.appendChild(bAddress);
+ card.appendChild(bEmail);
+ 
+ 
+  
+cards.appendChild(card);
+    }); // end of each loop
+  }; // end of function expression
+
+  getCompaniesData();
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector("article");
@@ -17,57 +71,4 @@ function showList() {
 	display.classList.remove("grid");
 }
 
-const url = 'https://jwesthora.github.io/wdd230/chamber/data.json';
 
-
-async function getCompaniesData() {
-    const response = await fetch(url);
-    const data = await data.json();
-    //console.table(data.prophets); // reference the prophet array of data object
-    displayCompanies(data.companies);
-}
-
-
-
-const displayCompanies = (companies) => {
-const cards = document.querySelector('div.cards'); // select the output container element
-  
-companies.forEach((companies) => {
- // Create elements to add to the div.cards element
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-    let bPhone = document.createElement('p');
-    let bEmail = document.createElement('p');
-    let bAddress = document.createElement('p');
-    
-  
-// Build the h2 content out to show the prophet's full name - finish the template string
-    h2.textContent = `${companies.bName}`;
-
-// Build the p content to show prophet's birth date and birth place
-bPhone.textContent = `Phone: ${companies.bPhone}`;
-bEmail.textContent = `Contact: ${companies.bEmail}`;
-bAddress.textContent = `Address: ${companies.bAddress}`;
-
- // Build the image portrait by setting all the relevant attribute
-    portrait.setAttribute('src', companies.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${companies.bName} ______________`);
-    portrait.setAttribute('loading', 'lazy');
-    portrait.setAttribute('width', '340');
-    portrait.setAttribute('height', '440');
-  
-// Append the section(card) with the created elements
- card.appendChild(h2);
- card.appendChild(portrait);
- card.appendChild(bPhone);
- card.appendChild(bAddress);
- card.appendChild(bEmail);
- 
- 
-  
-cards.appendChild(card);
-    }); // end of each loop
-  }; // end of function expression
-
-  getCompaniesData();
