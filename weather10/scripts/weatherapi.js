2026eca06e8da61fb99003b3dc8885a6affdbb8e
jwesthora <jud05001@byui.edu>
2023-03-06 11:41:15 -0800

    weather

---
+++ weather10/scripts/weatherapi.js
@@ -0,0 +1,36 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=2c06a713eee7a2303d487735ac244c3e&units=imperial`;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      // displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(weatherdata) {
+  temp.innerHTML = `<strong>${weatherdata.main.temp.toFixed(0)}</strong>`
+  const iconsrc = `https://openweathermap.org/img/w/${weatherdata.weather[0].icon}.png`;
+  const desc = weatherdata.weather[0].description;
+
+  wIcon.setAttribute('src', iconsrc);
+  wIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+
+
+
+}
