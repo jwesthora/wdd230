7f7099a6c6ead3b528ab7bf9aa5536a70324617c
jwesthora <jud05001@byui.edu>
2023-03-07 19:45:40 -0800

    fix temp

--- weather10/scripts/weatherapi.js
+++ weather10/scripts/weatherapi.js
@@ -1,5 +1,5 @@
 // select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
+const temp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
