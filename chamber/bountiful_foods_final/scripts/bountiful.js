b9461f0bf4344c8c8ec1b68c51b98f43bb0be13a
jwesthora <jud05001@byui.edu>
2023-03-22 15:53:13 -0700

    saving

--- chamber/bountiful_foods_final/scripts/bountiful.js
+++ chamber/bountiful_foods_final/scripts/bountiful.js
@@ -1,53 +1,36 @@
-const url = 'https://jwesthora.github.io/wdd230/chamber/data.json';
+const url = 'https://github.com/jwesthora/wdd230/blob/main/chamber/bountiful_foods_final/scripts/drink.json';
 
 
-async function getCompaniesData() {
+async function getBountifulData() {
     const response = await fetch(url);
     const data = await response.json();
     //console.table(data.companies); // reference the prophet array of data object
-    displayCompanies(data.companies);
+    displayBountiful(data.bountiful);
 
 }
 
 
 
-const displayCompanies = (companies) => {
+const displayBountiful = (bountiful) => {
 const cards = document.querySelector('div.cards'); // select the output container element
   
 companies.forEach((company) => {
  // Create elements to add to the div.cards element
     let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-    let bPhone = document.createElement('p');
-    let bEmail = document.createElement('p');
-    let bAddress = document.createElement('p');
-    let memberShip = document.createElement(`p`);
+    let name = document.createElement('h2');
+    let nutritions = document.createElement(`p`);
     
   
-// Build the h2 content out to show the prophet's full name - finish the template string
-    h2.textContent = `${company.bName}`;
+// Build the h2 content out to show the fruits name 
+    h2.textContent = `${bountiful.name}`;
 
-// Build the p content to show phone, address, URL(email)
-bPhone.textContent = `Phone: ${company.bPhone}`;
-bEmail.textContent = `${company.bEmail}`;
-bAddress.textContent = `Address: ${company.bAddress}`;
-memberShip.textContent =`Membership Status: ${company.memberShip}`;
+// Build the p content to show nutrition
+nutritionS.textContent =`Nutrition Information: ${bountiful.nutritions}`;
 
- // Build the image portrait by setting all the relevant attribute
-    portrait.setAttribute('src', company.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${company.bName} ______________`);
-    portrait.setAttribute('loading', 'lazy');
-    portrait.setAttribute('width', '300');
-    portrait.setAttribute('height', '200');
   
 // Append the section(card) with the created elements
  card.appendChild(h2);
- card.appendChild(portrait);
- card.appendChild(bPhone);
- card.appendChild(bAddress);
- card.appendChild(bEmail);
- card.appendChild(memberShip);
+ card.appendChild(nutritions);
  
  
   
@@ -55,7 +38,7 @@ cards.appendChild(card);
     }); // end of each loop
   }; // end of function expression
 
-  getCompaniesData();
+  getBountifulData();
 const gridbutton = document.querySelector("#squareView");
 const listbutton = document.querySelector("#listView");
 const display = document.querySelector(".cards");
